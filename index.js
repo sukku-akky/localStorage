56eb784ab6f62efb5c3ff3172b2f59cd38756a76
sukku-tech <cherukupallisukanya808gmail.com>
2024-06-26 17:48:34 +0530

    editFunctionality

--- index.js
+++ index.js
@@ -23,11 +23,23 @@ form.addEventListener("submit",function(event){
     const button=document.createElement("input");
     button.value="Delete";
     button.type="button";
+    const edit=document.createElement("input");
+    edit.type="button";
+    edit.value="Edit";
     newLi.appendChild(button);
+    newLi.appendChild(edit);
     button.addEventListener("click",function(event){
         event.preventDefault();
         list.removeChild(button.parentNode);
     });
+    edit.addEventListener("click",function(event){
+        event.preventDefault();
+        localStorage.removeItem(emailValue);
+        list.removeChild(button.parentNode);
+        document.getElementById('username').value = name;
+        document.getElementById('email').value=emailValue;
+        document.getElementById("phone").value=numberValue;
+    })
 
 
 
